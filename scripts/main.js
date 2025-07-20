94ea19ca372d5d4578931bb35b07ebadd0c9ceec
doverglide <netesov2333@mail.ru>
2025-07-20 23:27:57 +0300

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,15 @@
+const telegramUser = window.Telegram.WebApp.initDataUnsafe.user;
+
 const API = 'https://parshop-miniapp.vercel.app/api/register';
 
 fetch(API, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ 
-        telegramUserId: user.id,
-        username:       user.username
+        telegramUserId: telegramUser.id,
+        username: telegramUser.username
     }),
 })
-
 .then(res => res.json())
 .then(data => {
     console.log('Пользователь зарегистрирован:', data.user);
