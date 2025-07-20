806d38abc4d00e5952c9c3290ee497709ade12e0
doverglide <netesov2333@mail.ru>
2025-07-20 23:23:19 +0300

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,12 @@
-fetch('/api/register', {
+const API = 'https://parshop-miniapp.vercel.app/api/register';
+
+fetch(API, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-        telegramUserId: window.Telegram.WebApp.initDataUnsafe.user.id,
-        username:       window.Telegram.WebApp.initDataUnsafe.user.username
-    })
+    body: JSON.stringify({ 
+        telegramUserId: user.id,
+        username:       user.username
+    }),
 })
 
 .then(res => res.json())
